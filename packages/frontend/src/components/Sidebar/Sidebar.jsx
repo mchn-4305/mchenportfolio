aae6e0446d30b8b062ff885888d321c63bac0b64
Micah Chen <micahchen34@gmail.com>
2025-04-06 11:59:48 -0700

    feat: universal styles, added links to bottom o f nav

--- packages/frontend/src/components/Sidebar/Sidebar.jsx
+++ packages/frontend/src/components/Sidebar/Sidebar.jsx
@@ -2,6 +2,7 @@ import styles from "./Sidebar.module.scss"
 import { Link, NavLink } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
 const Sidebar = () => {
     return (
@@ -11,7 +12,9 @@ const Sidebar = () => {
                 <h3 className={styles.subLogo}>Micah Chen</h3>
             </Link>
             <nav className={styles.nav}>
-                <NavLink className={styles.a} exact="true" activeclassname="active" to="/">
+                <NavLink className={({ isActive }) =>
+                        isActive ? `${styles.a} ${styles.active}` : styles.a
+                    } exact="true"  to="/">
                     <FontAwesomeIcon className={styles.i} icon={faHome} color="#4d4d4e"></FontAwesomeIcon>
                 </NavLink>
                 <NavLink  className={`${styles.a} ${styles.aboutLink}`} exact="true" activeclassname="active" to="/about">
@@ -21,6 +24,32 @@ const Sidebar = () => {
                     <FontAwesomeIcon className={styles.i} icon={faEnvelope} color="#4d4d4e"></FontAwesomeIcon>
                 </NavLink>
             </nav>
+            <ul>
+                <li>
+                    <a 
+                        target="_blank" 
+                        rel="noreferrer"
+                        href="https://www.linkedin.com/in/micah-chen-9321a3267/">
+                        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"></FontAwesomeIcon>
+                    </a>
+                </li>
+                <li>
+                    <a 
+                        target="_blank" 
+                        rel="noreferrer"
+                        href="https://github.com/mchn-4305/">
+                        <FontAwesomeIcon icon={faGithub} color="#4d4d4e"></FontAwesomeIcon>
+                    </a>
+                </li>
+                <li>
+                    <a 
+                        target="_blank" 
+                        rel="noreferrer"
+                        href="https://www.instagram.com/micahchen_ca/">
+                        <FontAwesomeIcon icon={faInstagram} color="#4d4d4e"></FontAwesomeIcon>
+                    </a>
+                </li>
+            </ul>
        </div>
     )
 }
